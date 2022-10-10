e6c5e8b3fe8b7e1980937d58e77cf1da36750f36
Deniz <dennos746@gmail.com>
2022-10-11 01:03:41 +0300

    updaeee

--- main.py
+++ main.py
@@ -58,6 +58,7 @@ class Tools:
         if want_proxy: self.proxies = open('proxies.txt','r',encoding='utf-8').read().splitlines()
         self.usernames = open('Scrapes/usernames.txt','r',encoding='utf-8').read().splitlines()
         self.token = json.load(open('config.json','r',encoding='utf-8'))['account_token']
+        self.xsup = 'eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzEwNi4wLjAuMCBTYWZhcmkvNTM3LjM2IiwiYnJvd3Nlcl92ZXJzaW9uIjoiMTA2LjAuMC4wIiwib3NfdmVyc2lvbiI6IjEwIiwicmVmZXJyZXIiOiIiLCJyZWZlcnJpbmdfZG9tYWluIjoiIiwicmVmZXJyZXJfY3VycmVudCI6IiIsInJlZmVycmluZ19kb21haW5fY3VycmVudCI6IiIsInJlbGVhc2VfY2hhbm5lbCI6InN0YWJsZSIsImNsaWVudF9idWlsZF9udW1iZXIiOjE1MTYzOCwiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbH0='
         
         self.scraped_counter = 0
         self.param = {
@@ -82,15 +83,13 @@ class Tools:
         while True:
             try:
                 headers = {
-                    'authority': 'discord.com',
                     'accept': '*/*',
                     'authorization': self.token,
                     'cache-control': 'no-cache',
                     'pragma': 'no-cache',
                     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
                     'x-debug-options': 'bugReporterEnabled',
-                    'x-discord-locale': 'tr',
-                    'x-super-properties': 'eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6InRyLVRSIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzEwNi4wLjAuMCBTYWZhcmkvNTM3LjM2IiwiYnJvd3Nlcl92ZXJzaW9uIjoiMTA2LjAuMC4wIiwib3NfdmVyc2lvbiI6IjEwIiwicmVmZXJyZXIiOiIiLCJyZWZlcnJpbmdfZG9tYWluIjoiIiwicmVmZXJyZXJfY3VycmVudCI6IiIsInJlZmVycmluZ19kb21haW5fY3VycmVudCI6IiIsInJlbGVhc2VfY2hhbm5lbCI6InN0YWJsZSIsImNsaWVudF9idWlsZF9udW1iZXIiOjE1MDc0OCwiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbH0=',
+                    'x-super-properties': self.xsup,
                 }
                 r = requests.get(f'https://discord.com/api/v9/channels/{channelid}/messages',params=self.param,headers=headers)
 
@@ -108,8 +107,23 @@ class Tools:
                         if want_proxy:
                             proxy = choice(self.proxies)
                             proxies = {
-                                "http": f"http://{proxy}"
+                                "http": f'http://{proxy}'
                             }
+                            
+                            headers = {
+                                "accept": "*/*",
+                                "accept-encoding": "gzip, deflate, br",
+                                'authorization': self.token,
+                                "dnt": "1",
+                                "sec-fetch-dest": "empty",
+                                "sec-fetch-mode": "cors",
+                                "sec-fetch-site": "same-origin",
+                                "sec-gpc": "1",
+                                "x-debug-options": "bugReporterEnabled",
+                                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+                                "x-super-properties": self.xsup
+                            }
+                            
                             r3 = requests.get(url=f'https://discord.com/api/v9/users/{userid}/profile',headers=headers, proxies=proxies, timeout=10)
                             try:
                                 userbio = r3.json()['user']['bio']
@@ -117,7 +131,8 @@ class Tools:
                                 print(f'{self.b}[{self.p}Debug Mode{self.b}] {self.w}[{self.y}i{self.w}] Error -> {self.r}No biography')
                                 userbio = False
                             except requests.exceptions.JSONDecodeError as e:
-                                print(f'{self.b}[{self.p}Debug Mode{self.b}] {self.w}[{self.y}i{self.w}] Error -> {self.r}Biography Decode Error')
+                                print(r3.text)
+                                print(f'{self.b}[{self.p}Debug Mode{self.b}] {self.w}[{self.y}i{self.w}] Error -> {self.r}Rate Limited!')
                                 userbio = False
                                 
                         if want_proxy:
@@ -150,7 +165,6 @@ class Tools:
                     sleep(10)
                     break
                 else:
-                    print(r.status_code)
                     print(f'{self.b}[{self.p}Debug Mode{self.b}] {self.w}[{self.y}i{self.w}] Error -> {self.r}{str(e).capitalize()}')
                 sleep(5)
 
